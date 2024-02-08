008cdda6a75ea0a429299d017f03281cf44a6c68
btwbeatriz <beatriz.ffreitas@hotmail.com>
2024-02-08 19:44:30 -0300

    Criando repository, entidades e seed

--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,19 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto){
-        if (dto.getEmail().equals("beatriz@email.com") && dto.getTechnology().contentEquals("Java")){
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()){
             return true;
         }
         return false;
